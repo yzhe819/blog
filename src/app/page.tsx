f058e26cccd30b1332749f7a548c16ad0252aa5a
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2025-12-02 02:10:41 +1300

    chore: content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,18 +158,16 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  Hackathons
+                  Footprints
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
                   I like building things
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  During my time in university, I attended{" "}
-                  {DATA.hackathons.length}+ hackathons. People from around the
-                  country would come together and build incredible things in 2-3
-                  days. It was eye-opening to see the endless possibilities
-                  brought to life by a group of motivated and passionate
-                  individuals.
+                  I attended {DATA.hackathons.length}+ web3 projects. People
+                  from around the country would come together and build
+                  incredible things in internet space. Here are some of my
+                  favorite projects.
                 </p>
               </div>
             </div>
